453807927f2a2c6b371a8ada45cbba6211f8a63b
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-05-20 20:36:29 +0530

    working. don't touch

--- client/src/Components/Options.js
+++ client/src/Components/Options.js
@@ -74,6 +74,7 @@ const Options = ({ children }) => {
 								onChange={(e) => setName(e.target.value)}
 								fullWidth
 							/>
+							{console.log(me)}
 							<CopyToClipboard
 								text={me}
 								className={classes.margin}
